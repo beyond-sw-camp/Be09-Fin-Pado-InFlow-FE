3925290023f99cbcb113b0d82e3c980353af8db1
Kibeom Jeon <kibeom1145@gmail.com>
2024-12-08 17:49:01 +0900

    Test: 프론트 배포 테스트 17:48

--- server.js
+++ server.js
@@ -4,21 +4,17 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/health', (req, res) => {
-  res.status(200).send('OK');
-});
-
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
